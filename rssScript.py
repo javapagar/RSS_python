945a8bcebc3fb862c5e5d5866d36c25a7c23df33
captainvinager <captainvinager@gmail.com>
2021-02-21 14:54:14 +0100

    todos los periodicos soporte RSS y Atom

--- rssScript.py
+++ rssScript.py
@@ -1,15 +1,55 @@
-import pandas as pd
+import feedparser
 import requests
-import xml.etree.ElementTree as xmlET
+import xml.etree.ElementTree as eltree
 
-url = 'https://e00-expansion.uecdn.es/rss/empresasdigitech.xml'
+expansion = 'https://e00-expansion.uecdn.es/rss/empresasdigitech.xml'
+economista ='https://www.eleconomista.es/rss/rss-empresas.php'
+cincodias ='https://cincodias.elpais.com/seccion/rss/companias/'
+confidencial = 'https://rss.elconfidencial.com/empresas/'
 
-request = requests.get(url)
-
-xmlPortada = xmlET.ElementTree(request.content)
-
-root = xmlPortada.getroot()
+response = requests.get(confidencial)
+tree = eltree.fromstring(response.content)
+items = tree.findall('channel/item')
 
 
+if items == []:
+    feed = feedparser.parse(confidencial)
+    items = feed.entries
+
+itemsFiltrados = []
+
+for item in items:
+
+    part = item.title#formato para atom
+
+    descripcion = item.findtext('description')#formato para rss
+
+    if 'Telefónica' in part or 'Caixa' in part:
+        itemsFiltrados.append(item)
+
+print(itemsFiltrados)
+
+
+def getXML(url, atom):
+
+    itemsFiltrados = []
+
+
+    try:
+        response = requests.get(url)
+
+        tree = eltree.fromstring(response.content)
+
+        items = tree.findall('channel/item')
+
+        for item in items:
+            categories = item.findall('category')
+            for category in categories:
+                if 'Telefónica' in category.text:
+                    itemsFiltrados.append(item)
+    except:
+        pass
+
+    return itemsFiltrados
 
 
